a5d7961c03e21b3b4e943f612217cd572834dbfe
yanghao <yanghao@baimaohui.net>
2022-08-12 11:56:56 +0800

    separate bind body/query/header

--- params.go
+++ params.go
@@ -32,9 +32,33 @@ func String(key string, errRet BusError) func(*gin.Context) (string, error) {
 	}
 }
 
-func Bind[T any](param T, errRet BusError) func(*gin.Context) (T, error) {
+func BindBody[T any](param T, errRet BusError) func(*gin.Context) (T, error) {
 	return func(ctx *gin.Context) (T, error) {
 		err := ctx.ShouldBind(&param)
+		if err != nil {
+			return param, errRet.Wrap(err)
+
+		}
+
+		return param, nil
+	}
+}
+
+func BindQuery[T any](param T, errRet BusError) func(*gin.Context) (T, error) {
+	return func(ctx *gin.Context) (T, error) {
+		err := ctx.ShouldBindQuery(&param)
+		if err != nil {
+			return param, errRet.Wrap(err)
+
+		}
+
+		return param, nil
+	}
+}
+
+func BindHeader[T any](param T, errRet BusError) func(*gin.Context) (T, error) {
+	return func(ctx *gin.Context) (T, error) {
+		err := ctx.ShouldBindHeader(&param)
 		if err != nil {
 			return param, errRet.Wrap(err)
 
