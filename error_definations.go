003dcbaf387f71eb42c1652c63ad581cde5b8fcb
yanghao05 <yanghao05@qianxin.com>
2023-06-13 18:42:44 +0800

    modify define

--- error_definations.go
+++ error_definations.go
@@ -3,8 +3,8 @@ package gen
 import "net/http"
 
 var (
-	PathParamError   = NewBusError(http.StatusBadRequest, 1001, "path param error")
-	QueryParamError  = NewBusError(http.StatusBadRequest, 1002, "query param error")
-	BodyParamError   = NewBusError(http.StatusBadRequest, 1003, "body param error")
-	HeaderParamError = NewBusError(http.StatusBadRequest, 1004, "header param error")
+	PathParamError   = NewBusError(http.StatusBadRequest, 1001, "path param error %s")
+	QueryParamError  = NewBusError(http.StatusBadRequest, 1002, "query param error %s")
+	BodyParamError   = NewBusError(http.StatusBadRequest, 1003, "body param error %s")
+	HeaderParamError = NewBusError(http.StatusBadRequest, 1004, "header param error %s")
 )
