393d8a75be9d5367c0976968ed1247abc63b0d7f
Rogee <rogeecn@qq.com>
2025-09-09 11:49:15 +0800

    feat: update

--- types/geometry_box.go
+++ types/geometry_box.go
@@ -1,14 +1,14 @@
 package types
 
 import (
-    "context"
-    "database/sql/driver"
-    "fmt"
-    "strings"
+	"context"
+	"database/sql/driver"
+	"fmt"
+	"strings"
 
-    "gorm.io/gorm"
-    "gorm.io/gorm/clause"
-    "gorm.io/gorm/schema"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"gorm.io/gorm/schema"
 )
 
 type Box struct{ P1, P2 Point }
@@ -35,12 +35,14 @@ func (b *Box) Scan(value interface{}) error {
 	*b = Box{P1: p1, P2: p2}
 	return nil
 }
+
 func (b Box) Value() (driver.Value, error) {
-    return fmt.Sprintf("(%g,%g),(%g,%g)", b.P1.X, b.P1.Y, b.P2.X, b.P2.Y), nil
+	return fmt.Sprintf("(%g,%g),(%g,%g)", b.P1.X, b.P1.Y, b.P2.X, b.P2.Y), nil
 }
 
 func (b Box) GormValue(ctx context.Context, db *gorm.DB) clause.Expr {
-    v, _ := b.Value(); return gorm.Expr("?", v)
+	v, _ := b.Value()
+	return gorm.Expr("?", v)
 }
 
 // Constructors
@@ -48,3 +50,6 @@ func NewBox(p1, p2 Point) Box { return Box{P1: p1, P2: p2} }
 
 // Edit helpers
 func (b *Box) Set(p1, p2 Point) { b.P1, b.P2 = p1, p2 }
+
+func (b Box) P1Point() Point { return b.P1 }
+func (b Box) P2Point() Point { return b.P2 }
